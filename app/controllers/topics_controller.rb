fc47e1e3dcca83694bdc386247d501d68e6eb1bf
Tyler <twr349@gmail.com>
2018-02-16 20:26:56 +0000

    destroy fixed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -47,7 +47,7 @@ class TopicsController < ApplicationController
         
       if @topic.destroy
        flash[:notice] = "\"#{@topic.title}\" was deleted successfully."
-       redirect_to subject_path(@subject)
+       redirect_to @subject
       else
        flash.now[:alert] = "There was an error deleting the topic."
        render :show
@@ -56,7 +56,7 @@ class TopicsController < ApplicationController
     
     private
       def topic_params
-        params.require(:topic).permit(:title, :body, :subject_id)
+        params.require(:topic).permit(:title, :body)
       end
    
 
